6c7dc47a6e12df5e961b667e3d55e33d63da33ab
liukeshao <liukeshao@gmail.com>
2017-04-22 13:08:43 +0800

    add web module

---
+++ totoro-web/src/main/kotlin/xyz/gosick/totoro/web/WebApplication.kt
@@ -0,0 +1,16 @@
+package xyz.gosick.totoro.web
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession
+
+/**
+ * Created by ks on 2017/4/21.
+ */
+@EnableRedisHttpSession
+@SpringBootApplication
+class WebApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(WebApplication::class.java, *args)
+}
