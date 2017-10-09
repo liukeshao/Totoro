4943fc4c5dacf5012e4dc193d93b682d0264de9e
liukeshao <liukeshao@gmail.com>
2017-10-09 16:25:43 +0800

    upgrade to webflux

--- totoro-web/src/main/kotlin/xyz/gosick/totoro/web/controller/UserController.kt
+++ totoro-web/src/main/kotlin/xyz/gosick/totoro/web/controller/UserController.kt
@@ -3,9 +3,10 @@ package xyz.gosick.totoro.web.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.WebSession
 import xyz.gosick.totoro.user.mapper.UserMappper
 import xyz.gosick.totoro.user.model.User
-import javax.servlet.http.HttpSession
+import java.util.*
 
 /**
  * Created by ks on 2017/4/22.
@@ -15,9 +16,9 @@ import javax.servlet.http.HttpSession
 class UserController(val userMappper: UserMappper) {
 
     @GetMapping
-    fun getUser(session: HttpSession): User {
-        session.setAttribute("name", "test")
-        return userMappper.findById(1L)
+    fun getUser(session: WebSession): User {
+        session.attributes.put("name", "test")
+        return User(1L, "test", "test", "test", "test", "test", 1, 1, "test", "test", Date(), Date())
     }
 
     @GetMapping("/test")
