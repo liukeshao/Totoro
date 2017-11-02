bddb0cfcc514e37b6376224588b814e6f7cff048
liukeshao <liukeshao@gmail.com>
2017-11-02 17:29:33 +0800

    change database to postgresql

--- totoro-user/src/main/resources/db/migration/V1_0__create.sql
+++ totoro-user/src/main/resources/db/migration/V1_0__create.sql
@@ -9,18 +9,29 @@
 
 -- totoro 用户表
 -- 2017-04-22 初次创建
-CREATE TABLE IF NOT EXISTS `totoro_users` (
-  `id`              BIGINT        NOT NULL AUTO_INCREMENT  COMMENT '自增主键',
-  `nickname`        VARCHAR(20)   NOT NULL DEFAULT ''      COMMENT '用户名',
-  `display_name`    VARCHAR(32)   NOT NULL DEFAULT ''      COMMENT '用户的显示名',
-  `password`        VARCHAR(32)   NOT NULL DEFAULT ''      COMMENT '密码',
-  `mobile`          VARCHAR(11)   NOT NULL DEFAULT ''      COMMENT '手机号',
-  `email`           VARCHAR(100)  NOT NULL DEFAULT ''      COMMENT '邮箱',
-  `type`            SMALLINT      NOT NULL DEFAULT 0       COMMENT '用户类型',
-  `status`          SMALLINT      NOT NULL DEFAULT 0       COMMENT '用户状态',
-  `out_id`          VARCHAR(32)   NOT NULL DEFAULT ''      COMMENT '关联的外部用户ID',
-  `out_type`        VARCHAR(32)   NOT NULL DEFAULT ''      COMMENT '用户来源',
-  `created_at`      DATETIME      NOT NULL DEFAULT now()   COMMENT '创建时间',
-  `updated_at`      DATETIME      NOT NULL DEFAULT now()   COMMENT '更新时间',
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS totoro_users
+(
+  id           BIGSERIAL                                    NOT NULL
+  CONSTRAINT totoro_users_pkey
+  PRIMARY KEY,
+  nickname     VARCHAR(32) DEFAULT '' :: CHARACTER VARYING  NOT NULL,
+  display_name VARCHAR(32) DEFAULT '' :: CHARACTER VARYING  NOT NULL,
+  password     VARCHAR(128) DEFAULT '' :: CHARACTER VARYING NOT NULL,
+  mobile       VARCHAR(16) DEFAULT '' :: CHARACTER VARYING  NOT NULL,
+  email        VARCHAR(32) DEFAULT '' :: CHARACTER VARYING  NOT NULL,
+  type         SMALLINT DEFAULT 0                           NOT NULL,
+  status       SMALLINT DEFAULT 0                           NOT NULL,
+  create_time  TIMESTAMP DEFAULT CURRENT_TIMESTAMP          NOT NULL,
+  update_time  TIMESTAMP DEFAULT CURRENT_TIMESTAMP          NOT NULL
 );
+COMMENT ON TABLE totoro_users IS '用户表';
+COMMENT ON COLUMN totoro_users.id IS '自增主键';
+COMMENT ON COLUMN totoro_users.nickname IS '用户名';
+COMMENT ON COLUMN totoro_users.display_name IS '用户的显示名';
+COMMENT ON COLUMN totoro_users.password IS '密码';
+COMMENT ON COLUMN totoro_users.mobile IS '手机号';
+COMMENT ON COLUMN totoro_users.email IS '邮箱';
+COMMENT ON COLUMN totoro_users.type IS '用户类型';
+COMMENT ON COLUMN totoro_users.status IS '用户状态';
+COMMENT ON COLUMN totoro_users.create_time IS '创建时间';
+COMMENT ON COLUMN totoro_users.update_time IS '更新时间';
